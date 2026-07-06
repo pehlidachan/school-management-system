8336ed5c4b9ff42f5580a5610f8f76f98887ac1b
Muhammad Naveed Bhatti <119340727+pehlidachan@users.noreply.github.com>
2026-07-06 11:02:42 +0500

    Stability: add database table checks to URL checker

--- school/management/commands/check_school_urls.py
+++ school/management/commands/check_school_urls.py
@@ -1,4 +1,6 @@
+from django.apps import apps
 from django.core.management.base import BaseCommand
+from django.db import connection
 from django.template.loader import get_template
 from django.urls import NoReverseMatch, reverse
 
@@ -19,9 +21,16 @@ TEMPLATES = [
     'multi_child_guardians.html',
 ]
 
+MODEL_LABELS = [
+    'school.Student', 'school.Staff', 'school.AttendanceSession', 'school.StudentAttendance',
+    'school.FeeInvoice', 'school.SchoolExpense', 'school.Notice', 'school.SchoolCalendarEvent',
+    'school.LibraryBook', 'school.LibraryIssue', 'school.MessageThread', 'school.ThreadMessage',
+    'school.Exam', 'school.StudentMark', 'school.GatePass', 'school.NonTeachingStaff',
+]
+
 
 class Command(BaseCommand):
-    help = 'Checks important school app URL names and templates.'
+    help = 'Checks important school app URL names, templates and database tables.'
 
     def handle(self, *args, **options):
         errors = []
@@ -39,6 +48,20 @@ class Command(BaseCommand):
             except Exception as exc:
                 errors.append('TEMPLATE FAIL: ' + template_name + ' -> ' + str(exc))
                 self.stdout.write(self.style.ERROR(errors[-1]))
+        existing_tables = set(connection.introspection.table_names())
+        for label in MODEL_LABELS:
+            try:
+                app_label, model_name = label.split('.')
+                model = apps.get_model(app_label, model_name)
+                table_name = model._meta.db_table
+                if table_name in existing_tables:
+                    self.stdout.write(self.style.SUCCESS('TABLE OK: ' + table_name))
+                else:
+                    errors.append('TABLE FAIL: ' + table_name + ' missing')
+                    self.stdout.write(self.style.ERROR(errors[-1]))
+            except Exception as exc:
+                errors.append('MODEL FAIL: ' + label + ' -> ' + str(exc))
+                self.stdout.write(self.style.ERROR(errors[-1]))
         if errors:
             raise SystemExit(1)
-        self.stdout.write(self.style.SUCCESS('School URL/template check passed.'))
+        self.stdout.write(self.style.SUCCESS('School URL/template/table check passed.'))
